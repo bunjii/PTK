f38478ea74a42f978b6b82e9f9011f3c9add913e
bunjii <izumi@uirp.net>
2018-02-22 16:53:18 +0900

    Extract Elem, Extract Node and Select Node

--- PTKTEST11/TestC.cs
+++ PTKTEST11/TestC.cs
@@ -1,19 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Windows.Forms;
 
+using Grasshopper.GUI;
+using Grasshopper.GUI.Canvas;
 using Grasshopper.Kernel;
 using Grasshopper.Kernel.Types;
+
 using Rhino.Geometry;
 
 namespace PTK
 {
-    public class TestC : GH_Component
+    public class PTK3 : GH_Component
     {
+
         /// <summary>
         /// Initializes a new instance of the TestC class.
         /// </summary>
-        public TestC()
+        public PTK3()
           : base("3", "3",
               "Test component no.3: Decompose Element",
               "PTK", "STR")
@@ -34,11 +39,12 @@ namespace PTK
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddLineParameter("Line", "Line", "Line", GH_ParamAccess.list);
+            pManager.AddLineParameter("Lines", "Lines", "Lines", GH_ParamAccess.list);
             pManager.AddIntegerParameter("PTK ELEM ID", "PTK E ID", "PTK ELEM ID", GH_ParamAccess.list);
             pManager.AddIntegerParameter("PTK NODE ID 0", "PTK N0 ID", "PTK NODE ID 0", GH_ParamAccess.list);
             pManager.AddIntegerParameter("PTK NODE ID 1", "PKT N1 ID", "PTK NODE ID 1", GH_ParamAccess.list);
         }
+
 
         /// <summary>
         /// This is the method that actually does the work.
@@ -77,6 +83,10 @@ namespace PTK
             DA.SetDataList(2, n0ids);
             DA.SetDataList(3, n1ids);
             #endregion
+
+            #region messagebox
+            this.Message = "P T K";
+            #endregion
         }
 
         /// <summary>
@@ -99,5 +109,92 @@ namespace PTK
         {
             get { return new Guid("891a0366-cf2f-4642-b92b-4a93d0389330"); }
         }
+
+
+
+        // DDL custom attributes, etc
+        /*
+        public override void AppendAdditionalMenuItems(ToolStripDropDown menu)
+        {
+            base.AppendAdditionalComponentMenuItems(menu);
+            Menu_AppendSeparator(menu);
+            Menu_AppendItem(menu, "general", Menu_DoClick);
+            Menu_AppendItem(menu, "post-process");
+            Menu_AppendSeparator(menu);
+        }
+
+        private void Menu_DoClick(object sender, EventArgs e)
+        {
+            myBool = !myBool;
+        }
+        public bool myBool = false;
+        */
+
+        /*
+        public override void CreateAttributes()
+        {
+            m_attributes = new Attributes_Custom(this);
+        }
+        */
     }
+
+    /*
+    public class Attributes_Custom : Grasshopper.Kernel.Attributes.GH_ComponentAttributes
+    {
+        public Attributes_Custom(GH_Component owner) : base(owner)
+        {
+        }
+        
+        // 1. making a layout
+        protected override void Layout()
+        {
+            base.Layout();
+
+            // rec0: area for the component incl. extended area
+            Rectangle rec0 = GH_Convert.ToRectangle(Bounds);
+            rec0.Height += 30;
+
+            // rec1: only the extended area for buttons
+            Rectangle rec1 = rec0;
+            rec1.Y = rec1.Bottom - 30;
+            rec1.Height = 30;
+            rec1.Inflate(-2, -2);
+
+            Bounds = rec0;
+            ButtonBounds = rec1;
+        }
+        private Rectangle ButtonBounds { get; set; }
+
+        // 2. render
+        protected override void Render(GH_Canvas canvas, Graphics graphics, GH_CanvasChannel channel)
+        {
+            base.Render(canvas, graphics, channel);
+
+            if (channel == GH_CanvasChannel.Objects)
+            {
+                GH_Capsule button = GH_Capsule.CreateTextCapsule(ButtonBounds, ButtonBounds, GH_Palette.Pink, "Button", 2, 0);
+                button.Render(graphics, Selected, Owner.Locked, false);
+                button.Dispose();
+            }
+
+        }
+
+        // 3. define a mouse click event
+        public override GH_ObjectResponse RespondToMouseDown(GH_Canvas sender, GH_CanvasMouseEvent e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                RectangleF rec = ButtonBounds;
+                if (rec.Contains(e.CanvasLocation))
+                {
+                    // MessageBox.Show("The button was clicked", "Button", MessageBoxButtons.OK);
+                    return GH_ObjectResponse.Handled;
+                }
+            }
+            return base.RespondToMouseDown(sender, e);
+        }
+
+    }
+    */
+
 }
---
+++ PTKTEST11/TestD.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+
+using Rhino;
+using Rhino.DocObjects;
+using Rhino.Collections;
+
+using GH_IO;
+using GH_IO.Serialization;
+using Grasshopper;
+using Grasshopper.Kernel.Data;
+
+using System.IO;
+using System.Linq;
+using System.Drawing;
+using System.Reflection;
+using System.Collections;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace PTK
+{
+    public class PTK4 : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the TestD class.
+        /// </summary>
+        public PTK4()
+          : base("4", "4",
+              "Test component no.4: Decompose Node (Extract Node)",
+              "PTK", "STR")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("PTK NODE", "PTK N", "PTK NODE", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddPointParameter("Points", "Points", "Points", GH_ParamAccess.list);
+            pManager.AddIntegerParameter("PTK NODE ID", "PTK N ID", "PTK NODE ID", GH_ParamAccess.list);
+            pManager.AddIntegerParameter("PTK NODE ELEM ID", "PTK N E ID", "PTK NODE ELEM ID", GH_ParamAccess.tree);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            #region variables
+            GH_ObjectWrapper wrapNode = new GH_ObjectWrapper();
+            List<Node> nodes = new List<Node>();
+
+            List<Point3d> points = new List<Point3d>();
+            List<int> nodeIds = new List<int>();
+            DataTree<int> elemIdTree = new DataTree<int>();
+            #endregion
+
+            #region input
+            if (!DA.GetData(0, ref wrapNode)) { return; }
+            wrapNode.CastTo<List<Node>>(out nodes);
+            #endregion
+
+            #region solve
+            // foreach (Node n in nodes)
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                points.Add(nodes[i].Pt3d);
+                nodeIds.Add(nodes[i].ID);
+                GH_Path path = new GH_Path(i);
+                foreach (int j in nodes[i].ElemIds)
+                {
+                    elemIdTree.Add(j, path);
+
+                }
+            }
+            #endregion
+
+            #region output
+            DA.SetDataList(0, points);
+            DA.SetDataList(1, nodeIds);
+            DA.SetDataTree(2, elemIdTree);
+            #endregion
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return PTKTEST11.Properties.Resources.icon_truss;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("dd8adcf2-521c-44a4-8448-f0335469c0dd"); }
+        }
+    }
+}
