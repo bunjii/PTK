5367db3de64884476c52e828179aecd197cd5dcd
bunjii <izumi@uirp.net>
2018-04-11 18:24:35 +0900

    WIP commit #42

---
+++ PTK/PTK_4_1_KarambaAssemble.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+using Karamba.Models;
+using Karamba.Elements;
+
+namespace PTK
+{
+    public class PTK_4_1_KarambaAssemble : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the PTK_4_1_KarambaAssemble class.
+        /// </summary>
+        public PTK_4_1_KarambaAssemble()
+          : base("KarambaAssemble", "Karamba A (PTK)",
+              "Creates Model information of Karamba",
+              "PTK", "Assemble")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.RegisterParam(new Param_Model(), "outModel", "outModel", "Assembled Karamba Model", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("7c6860fa-ee7b-4580-9f04-7bab9e325d8b"); }
+        }
+    }
+}
